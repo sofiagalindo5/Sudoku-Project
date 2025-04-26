4025318f5b9636cef55173dab8b4b3b380dea533
gabbypolito <g.polito@ufl.edu>
2025-04-26 11:27:59 -0400

    Added draw function to cell

--- cell.py
+++ cell.py
@@ -17,3 +17,32 @@ class Cell:
 
    def set_sketched_value(self, value):
        self.sketched_value = value
+
+   def draw(self):
+       SQUARE_SIZE = 600 // 9
+       BG_COLOR = (255, 255, 245)
+       LINE_COLOR = (245, 152, 66)
+       CROSS_COLOR = (66, 66, 66)
+       RED = (255, 0, 0)
+
+
+       x = self.col * SQUARE_SIZE
+       y = self.row * SQUARE_SIZE
+
+
+       pygame.draw.rect(self.screen, BG_COLOR, (x, y, SQUARE_SIZE, SQUARE_SIZE))
+       pygame.draw.rect(self.screen, LINE_COLOR, (x, y, SQUARE_SIZE, SQUARE_SIZE), 1)
+
+
+       font = pygame.font.SysFont("Arial", 40)
+       if self.value != 0:
+           text = font.render(str(self.value), True, CROSS_COLOR)
+           self.screen.blit(text, (x + SQUARE_SIZE/2 - text.get_width()/2, y + SQUARE_SIZE/2 - text.get_height()/2))
+       elif self.sketched_value != 0:
+           sketched_font = pygame.font.SysFont("Arial", 20)
+           text = sketched_font.render(str(self.sketched_value), True, (0, 0, 0))
+           self.screen.blit(text, (x + 5, y + 5))
+
+
+       if self.selected:
+           pygame.draw.rect(self.screen, RED, (x, y, SQUARE_SIZE, SQUARE_SIZE), 3)
