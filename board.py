334206d3e1716ad2b7ac1b5ce098362f5e741085
sofiagalindol <sgalindo@sccs.edu.bo>
2025-04-26 14:13:08 -0400

    Beginning of cell class

--- board.py
+++ board.py
@@ -64,3 +64,86 @@ class Board:
            col = int(x // SQUARE_SIZE)
            return (row, col)
        return None
+
+   def clear(self):
+       if self.selected_cell:
+           row, col = self.selected_cell
+           if self.original_board[row][col] == 0:
+               self.cells[row][col].set_cell_value(0)
+               self.cells[row][col].set_sketched_value(0)
+
+
+   def sketch(self, value):
+       if self.selected_cell:
+           row, col = self.selected_cell
+           self.cells[row][col].set_sketched_value(value)
+
+
+   def place_number(self, value):
+       if self.selected_cell:
+           row, col = self.selected_cell
+           if self.original_board[row][col] == 0:
+               self.cells[row][col].set_cell_value(value)
+               self.cells[row][col].set_sketched_value(0)
+
+
+   def reset_to_original(self):
+       for row in range(BOARD_ROWS):
+           for col in range(BOARD_COLS):
+               self.cells[row][col].set_cell_value(self.original_board[row][col])
+               self.cells[row][col].set_sketched_value(0)
+
+
+   def is_full(self):
+       for row in self.cells:
+           for cell in row:
+               if cell.value == 0:
+                   return False
+       return True
+
+
+   def update_board(self):
+       for row in range(BOARD_ROWS):
+           for col in range(BOARD_COLS):
+               self.board[row][col] = self.cells[row][col].value
+
+
+   def find_empty(self):
+       for row in range(BOARD_ROWS):
+           for col in range(BOARD_COLS):
+               if self.cells[row][col].value == 0:
+                   return (row, col)
+       return None
+
+
+   def check_board(self):
+       for i in range(BOARD_ROWS):
+           row_vals = []
+           col_vals = []
+           for j in range(BOARD_COLS):
+               row_val = self.cells[i][j].value
+               col_val = self.cells[j][i].value
+               if row_val in row_vals or col_val in col_vals:
+                   return False
+               if row_val != 0:
+                   row_vals.append(row_val)
+               if col_val != 0:
+                   col_vals.append(col_val)
+
+
+       for box_row in range(0, BOARD_ROWS, 3):
+           for box_col in range(0, BOARD_COLS, 3):
+               box_vals = []
+               for r in range(3):
+                   for c in range(3):
+                       val = self.cells[box_row + r][box_col + c].value
+                       if val in box_vals:
+                           return False
+                       if val != 0:
+                           box_vals.append(val)
+       return True
+
+
+
+
+
